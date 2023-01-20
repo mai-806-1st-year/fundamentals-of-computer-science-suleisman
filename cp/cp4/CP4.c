b16c44fc23e2b03cca4225cf636221864a548746
Serafim Shelaev <113765497+suleisman@users.noreply.github.com>
2023-01-20 09:38:05 +0300

    Create CP4.c

---
+++ cp/cp4/CP4.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <math.h>
+
+double epsilon() {
+    double eps = 1.0;
+    while (1 + eps / 2.0 != 1)
+        eps /= 2.0;
+    return eps;
+}
+
+double f_1 (double x) {
+    return cos(2/x) - 2*sin(1/x) + 1/x;
+}
+
+double f_2 (double x) {
+    return  sqrt(1 - (0.4*x*x)) - asin(x);
+}
+
+double f_2_derivative(double x) {
+    return -((2 * x)/(5 * sqrt(1 - 0.4*x*x)))-(1/sqrt(2-x*x));
+}
+
+
+
+double dichotomy(double (*f)(double), double a, double b, double relative_eps, double abs_eps) {
+    double x = (a + b) / 2;
+    if (f(a) * f(b) < 0) {
+        while (fabs(a - b) > fmax(relative_eps * fabs(x), abs_eps)) {
+            x = (a + b) / 2;
+            if (f(x) * f(a) < 0) {
+                b = x;
+            }
+            else {
+                a = x;
+            }
+        }
+        return x;
+    }
+    else {
+        return 0;
+    }
+}
+
+double iterations(double (*f)(double), double (*f_derivative)(double), double a, double b, double relative_eps, double abs_eps) {
+    double x = (a + b) / 2;
+    if (fabs(f_derivative(x)) < 1) 
+    {
+        while (fabs(f (x) - x) >= fmax(relative_eps * fabs(x), abs_eps)) {
+            if(f(x) < 0) {
+                break;
+            }
+            x = f(x);
+        }
+        return x;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main() {
+    double abs_eps = epsilon();
+    double relative_eps = sqrt(abs_eps);
+    printf("%10f\n", dichotomy(f_1, 1, 2, relative_eps, abs_eps));
+    printf("%10f\n", iterations(f_2, f_2_derivative, 0, 1, relative_eps, abs_eps));
+    return 0;
+}
