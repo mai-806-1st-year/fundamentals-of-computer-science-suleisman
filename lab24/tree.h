feec6db708e0edc19636aa2dcb5404db09346208
Serafim Shelaev <113765497+suleisman@users.noreply.github.com>
2023-07-07 19:57:35 +0300

    Update tree.h

--- lab24/tree.h
+++ lab24/tree.h
@@ -1,1 +1,31 @@
 
+#ifndef TREE_H
+#define TREE_H
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+#include "stackbin.h"
+#include "stack.h"
+
+typedef struct TreeNode TreeNode;
+
+typedef struct {
+    TreeNode *root;
+    size_t size;
+} Tree;
+
+void treeClear(Tree *tree);
+void treeCreate(Tree *tree);
+void treeDestroy(Tree *tree);
+void treePrint(Tree *tree);
+void treeFromPostfix(Tree *tree, Stack *postfix, StackBin *stack);
+void treeDelOnes(Tree *tree);
+bool treeF(Tree *tree);
+void treeInorder(Tree *tree);
+int treeCount(Tree *tree);
+
+#endif //TREE_H
