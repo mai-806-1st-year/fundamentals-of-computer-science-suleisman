0936207dbbe0fae26abce2ffdd371a8e071912c1
Serafim Shelaev <113765497+suleisman@users.noreply.github.com>
2023-07-10 19:18:21 +0300

    Update main.py

--- lab21/main.py
+++ lab21/main.py
@@ -1,7 +1,8 @@
 #рекурсивный обход и посчет числа выполнимых файлов в каталоге и поддиректориях
 import os
+
 k=0
-path='C:\\Users\\seraf\\OneDrive\\Рабочий стол\\lab21'
+
 def FileCrowling(path,k):
     for i in os.listdir(path):
         if os.path.isfile(path+'\\'+i):
@@ -9,5 +10,15 @@ def FileCrowling(path,k):
         if os.path.isdir(path+'\\'+i):
             return FileCrowling(path+'\\'+i,k)
     return k
-        
-print(FileCrowling(path,0))
+a=input()
+if(a[0] == '?'):
+    print('You must enter the path to the folder for which you want to perform the action')
+    print('Do you want to enter a path? Yes or No?')
+    b=input()
+    if(b[0] == 'N'):
+        path='C:\\Users\\seraf\\OneDrive\\Рабочий стол\\lab21'
+        print(FileCrowling(path,0))
+    else:
+        path=input()
+        print(FileCrowling(path,0))
+    
