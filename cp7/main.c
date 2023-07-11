0270a54c6f1e9a8d7b01f24acea5aad85b9d69f6
Serafim Shelaev <113765497+suleisman@users.noreply.github.com>
2023-07-11 22:06:11 +0300

    Update main.c

--- cp7/main.c
+++ cp7/main.c
@@ -1,1 +1,55 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "matrix.h"
+
+void printMenu() {
+    printf(
+           "1 - print simple matrix\n"
+           "2 - print matrix\n"
+           "3 - multiplication\n" 
+           "4 - exit\n");
+}
+
+
+
+int main(int argc, char* argv[]) {
+    FILE *file;
+    
+    if (argc == 2) {
+        file = fopen(argv[1], "r");
+    } else {
+          file = fopen("t.txt", "r");
+      }
+    Matrix matrix;
+    matrixCreate(&matrix);
+    matrixScan(file, &matrix);
+    printMenu();
+    int a;
+    while (1) {
+        scanf("%d", &a);
+       
+        if (a == 1) {
+            matrixPrint(&matrix);
+        } else if (a == 2) {
+                   matrixPrintTable(&matrix); 
+               } else if (a == 3) {
+
+                          size_t *arr[2];
+                          matrixGetSize(&matrix, arr);
+                          size_t n = *arr[0];
+                          int string[n];
+                          printf("enter vector (1x%ld)   ", n);
+                          for (int i = 0; i < n; ++i) 
+                          scanf("%d", &string[i]);
+                          func(&matrix, string);
+                      } else if (a == 4) {
+                                 matrixDestroy(&matrix);
+                                 fclose(file);
+                                 break;
+                             } else 
+                                   printf("incorrect parametr\n");
+                             
+    }
+}
